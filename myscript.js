907fea8e15ebf754aabaa72d76f772838f6b551f
SaadNoor555 <ssaadn.555@gmail.com>
2022-02-26 11:18:59 +0600

    initial commit

---
+++ myscript.js
@@ -0,0 +1,28 @@
+
+/*
+firstName = "saad";
+lastName = "Bond";
+
+document.write(firstName);
+*/
+
+var x = 100;
+
+if(x >= 80) {
+	document.write("A+")
+}
+else if(x >= 70) {
+	document.write("A")
+}
+else if(x >= 60) {
+	document.write("A-")
+}
+else {
+	document.write("F")
+}
+document.write("<br>");
+
+for(var i=1; i<100; i++)
+{
+}
+
