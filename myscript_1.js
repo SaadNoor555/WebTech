33268f3ab8d715a1903d74c93889b069f062d42f
saad <ssaadn.555@gmail.com>
2022-03-22 14:24:55 +0600

    from class

--- myscript_1.js
+++ myscript_1.js
@@ -2,4 +2,11 @@ firstname = "James";
 lastname = "Bond";
 fullname = firstname + " " + lastname;
 alert(fullname);
-		
+isBlah = confirm("input something");
+document.write(isBlah);
+NodeMine = document.createElement("h1");
+textBall = document.createTextNode("this is a baaaal");
+NodeMine.appendChild(textBall);
+document.body.appendChild(NodeMine);
+document.body.firstChild.appendChild(textBall);
+console.log(fullname);
